c154d2df9d7146629307683ae4096af5df788b63
doswald32 <dan.oswald32@gmail.com>
2024-12-02 21:24:09 -0700

    updated flask config file

--- server/app.py
+++ server/app.py
@@ -4,8 +4,11 @@
 
 # Remote library imports
 from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 # Local imports
 from config import app, db, api
@@ -13,8 +16,24 @@ from config import app, db, api
 # Add your model imports
 from models import db
 
-# Views go here!
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db=SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+CORS(app)
+
+# Views go here!
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
