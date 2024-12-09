45e230a3ca8f29bc64e660a3fbd1eccc5695d493
doswald32 <dan.oswald32@gmail.com>
2024-12-08 20:59:24 -0700

    created basic Login component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
-//testing
---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+
+
+function Login() {
+    return (
+        <main className="login-all">
+            <div className="login-window">
+                <h1>Sign in</h1>
+            </div>
+        </main>
+        
+    );
+};
+
+export default Login;
