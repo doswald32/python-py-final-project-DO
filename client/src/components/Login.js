7091aeb3d50f2bbe20c0725c384c4d6a9ab2a4d7
doswald32 <dan.oswald32@gmail.com>
2024-12-08 21:48:02 -0700

    basic framework for login screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,9 +2,20 @@
 
 function Login() {
     return (
-        <main className="login-all">
-            <div className="login-window">
-                <h1>Sign in</h1>
+        <main className="login-container">
+            <div className="login-window-border">
+                <div className="login-window">
+                    <h1>Sign in</h1>
+                    <form className="login-form">
+                        <input className="login-username" type="text" placeholder="Username or Email"/>
+                        <input className="login-password" type="text" placeholder="Password" />
+                        <p>Forgot password?</p>
+                        <button className="Login Button">Log In</button>
+                        <hr></hr>
+                        <p>New to Jaunt? Click below to create an account!</p>
+                        <button>Create Account</button>
+                    </form>
+                </div>
             </div>
         </main>
         
