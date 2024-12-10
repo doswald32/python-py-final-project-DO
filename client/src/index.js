fc21c73ec47612963d9146d32bf5a4434eec0859
doswald32 <dan.oswald32@gmail.com>
2024-12-10 04:01:42 -0700

    commented out issues to continue work

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./components/App"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes"
 
-const router = createBrowserRouter(routes);
+// Commenting out to continue working while issues persist
+// const router = createBrowserRouter(routes);
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<RouterProvider router={router} />);
+root.render(<App />)
+
+
+// root.render(<RouterProvider router={router} />);
